9f9c714d8cb7770954f3b80bace331364f529161
TheBenx03 <TheBenx03@gmail.com>
2024-06-05 15:18:54 -0400

    match init

--- Assets/Scripts/Channel/ChannelManager.cs
+++ Assets/Scripts/Channel/ChannelManager.cs
@@ -1,13 +1,21 @@
 using System;
+using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
 
 public class ChannelManager : MonoBehaviour
 {
+    public Dictionary<Guid, string> channels;
+    public Dictionary<NetworkConnectionToClient, Guid> players;
     
     [ServerCallback] 
-    void CreateMatch(GameObject manager){
+    public void CreateMatch(string name){
         Guid newMatchId = Guid.NewGuid();
-        
+        channels.Add(newMatchId, name);
+    }
+    [ServerCallback]
+    public void JoinMatch(NetworkConnectionToClient player, Guid matchId){
+        player.identity.GetComponent<NetworkMatch>().matchId = matchId;
+        players.Add(player, matchId);
     }
 }
